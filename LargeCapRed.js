0d9f58e2b4f6bf7e5af607df7c287de5802b5482
CenturyOrb <122176094+CenturyOrb@users.noreply.github.com>
2025-09-25 23:15:12 -0400

    Worked on backspace

--- LargeCapRed.js
+++ LargeCapRed.js
@@ -1,12 +1,23 @@
-import { useState } from 'react'
+import { useState, useContext } from 'react'
 import { Text, Pressable, StyleSheet } from 'react-native'
+import { AppContext } from './AppContext.js'
 import { textWhite, red_color, redDark_color } from './constants.js'
 
 export default function LargeCap({ children }) {
 	const [backgroundColor, setBackgroundColor] = useState({backgroundColor: red_color});		
+	const { wordleGrid, setWordleGrid } = useContext(AppContext);
     
     const handleKeyPressIn = () => {
     	setBackgroundColor({backgroundColor: redDark_color});
+		// find the current row, last letter	
+		const wordleGridCopy = structuredClone(wordleGrid);            				
+        const currentRow = wordleGridCopy.find(row => row[4] === ' '); 
+        if (!currentRow) { 
+        	console.log('all rows filled'); 
+        }
+        const index = currentRow.findLastIndex(letter => letter !== ' ' );	
+        if (index !== -1) { currentRow[index] = ' '}
+        setWordleGrid(wordleGridCopy);
     }
     
     const handleKeyPressOut = () => {

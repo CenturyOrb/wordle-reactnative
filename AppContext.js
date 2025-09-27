7dca69e36acdf8dce7150abc5bc43fc69e91145c
CenturyOrb <122176094+CenturyOrb@users.noreply.github.com>
2025-09-27 10:13:55 -0400

    wordle fetch in WordleGrid Component

--- AppContext.js
+++ AppContext.js
@@ -6,10 +6,11 @@ const AppProvider = ({ children }) => {
 	const [wordleGrid, setWordleGrid] = useState(Array(6).fill(null).map(() => Array(5).fill(' ')));	
 	const [lastEntry, setLastEntry] = useState(null);
 	const finishedRow = useRef(-1);
+	const wordle = useRef(null);
 
 	return(
 		<AppContext.Provider
-			value={{wordleGrid, setWordleGrid, finishedRow }}
+			value={{wordleGrid, setWordleGrid, finishedRow, wordle}}
 		> 
 			{children}
 		</AppContext.Provider>
--- WordleGrid.js
+++ WordleGrid.js
@@ -1,10 +1,23 @@
 import { useContext, useEffect } from 'react'
 import { StyleSheet, View } from 'react-native'
+import axios from 'axios'
 import { AppContext } from './AppContext.js'
 import Row from './Row.js'
 
 export default function WordleGrid() {
-	const { wordleGrid, setWordleGrid } = useContext(AppContext);
+	const { wordleGrid, wordle } = useContext(AppContext);
+	
+	// on mount fetch for word
+	useEffect(() => {
+		const fetchWordle = async () => {
+			const response = await axios.get('https://random-word-api.herokuapp.com/word',
+				{params: {length: 5}}
+			);
+			wordle.current = response.data[0];
+			console.log(wordle.current);
+		}
+		fetchWordle();
+	}, []);	
 
 	return (
 		<View style={styles.wordle}>            		
